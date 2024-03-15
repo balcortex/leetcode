0a922b7c72e8a00f6fd278af3195458a31c7fc37
Bal Cortex <balcortex@hotmail.com>
2024-03-15 17:06:00 -0600

    add: 0238 - medium

--- medium/0238_product_array_except_self.py
+++ medium/0238_product_array_except_self.py
@@ -3,15 +3,26 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        for n in nums:
-            prod *= n
+        n = len(nums)
 
-        a = [prod / n if n != 0 else prod for n in nums]
-        print(a)
-        return [prod / n if n != 0 else prod for n in nums]
+        left = [1] * n  # prefix sum
+        right = [1] * n  # postfix sum
+
+        # Start at second element
+        # the first element does not have any numbers to the left
+        for i in range(1, n):
+            left[i] = left[i - 1] * nums[i - 1]
+
+        # Start at the second element from the end
+        for i in range(-2, -n - 1, -1):
+            right[i] = right[i + 1] * nums[i + 1]
+
+        # The solution is prefix[i] * postfix[i]
+        return [l * r for (l, r) in zip(left, right)]
 
 
 sol = Solution()
+
+assert sol.productExceptSelf([2, 3, 4, 5, 6]) == [360, 240, 180, 144, 120]
 assert sol.productExceptSelf([1, 2, 3, 4]) == [24, 12, 8, 6]
 assert sol.productExceptSelf([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
